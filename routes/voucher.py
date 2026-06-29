a6d1d4ba93ef5f4da7a1ccdae96a8348aa01702e
malav <makwanamalav890@gmail.com>
2026-06-29 20:13:19 +0530

    Day 10: built sales voucher and transactions

--- routes/voucher.py
+++ routes/voucher.py
@@ -22,3 +22,29 @@ def create_purchase_voucher(voucher: VoucherCreate):
         "company_id": 1, # Mocked active company ID
         "status": "Purchase Voucher Successfully Posted"
     }
+
+@router.post("/sales", response_model=VoucherResponse)
+def create_sales_voucher(voucher: VoucherCreate):
+    
+    if voucher.type.lower() != "sales":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Invalid voucher type. Must be 'Sales'."
+        )
+
+
+    total_debits = sum(entry.debit_amount for entry in voucher.entries)
+    total_credits = sum(entry.credit_amount for entry in voucher.entries)
+    
+    if total_debits != total_credits:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Accounting Rule Violation: Total Debits must equal Total Credits."
+        )
+
+    
+    return {
+        "id": 2, # Mocking a new ID
+        "company_id": 1, # Mocked active company ID
+        "status": "Sales Voucher Successfully Posted and Inventory Reduced"
+    }
